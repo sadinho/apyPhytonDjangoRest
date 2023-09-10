64a96c869b4f41e3d5342a3d9391a2d9233f169d
Sadinho <drdimsade@gmail.com>
2023-09-10 03:10:34 -0300

    finalização de implementação jwt e proteção rotas

--- api/core/views.py
+++ api/core/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import CreditCard
 from .serializers import CreditCardSerializer
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
 class CreditcardViewSet(viewsets.ModelViewSet):
     queryset = CreditCard.objects.all()
     serializer_class = CreditCardSerializer
