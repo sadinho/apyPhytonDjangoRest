65d9c4f3785ca8216b902566d43c0578c4500bfc
Sadinho <drdimsade@gmail.com>
2023-09-10 03:27:36 -0300

    ajuste do serializers pegos no teste e ajuste dos testes unitarios

--- api/core/serializers.py
+++ api/core/serializers.py
@@ -14,17 +14,21 @@ class CreditCardSerializer(serializers.ModelSerializer):
         return fields
 
     def validate_exp_date(self, value):
-        # Validar a data de expiração
-        from datetime import date
-        current_date = date.today()
-        if value < current_date:
-            raise serializers.ValidationError("A data de expiração não pode ser anterior à data atual.")
+        # Verifique o método da solicitação
+        if self.context.get('request') and self.context['request'].method in ['POST', 'PUT']:
+            # Lógica de validação para POST e PUT
+            from datetime import date
+            current_date = date.today()
+            if value < current_date:
+                raise serializers.ValidationError("A data de expiração não pode ser anterior à data atual.")
         return value
 
     def validate_holder(self, value):
-        # Validar o titular
-        if len(value) < 3:
-            raise serializers.ValidationError("O titular deve ter mais de 2 caracteres.")
+        # Verifique o método da solicitação
+        if self.context.get('request') and self.context['request'].method in ['POST', 'PUT']:
+            # Lógica de validação para POST e PUT
+            if len(value) < 3:
+                raise serializers.ValidationError("O titular deve ter mais de 2 caracteres.")
         return value
 
     def validate_number(self, value):
@@ -38,8 +42,10 @@ class CreditCardSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Erro ao validar o número de cartão de crédito.")
 
     def validate_cvv(self, value):
-        # Validar o CVV
-        if value and (len(value) < 3 or len(value) > 4 or not value.isdigit()):
-            raise serializers.ValidationError("O CVV deve ter entre 3 e 4 dígitos numéricos.")
+        # Verifique o método da solicitação
+        if self.context.get('request') and self.context['request'].method in ['POST', 'PUT']:
+            # Lógica de validação para POST e PUT
+            if value and (len(value) < 3 or len(value) > 4 or not value.isdigit()):
+                raise serializers.ValidationError("O CVV deve ter entre 3 e 4 dígitos numéricos.")
         return value
 
--- api/core/tests.py
+++ api/core/tests.py
@@ -1,30 +1,27 @@
 from django.test import TestCase
-from django.urls import reverse
-from rest_framework import status
-from rest_framework.test import APIClient
+from django.test.client import RequestFactory
 from .models import CreditCard
 from .serializers import CreditCardSerializer
 
 class CreditCardSerializerTests(TestCase):
     def setUp(self):
-        self.client = APIClient()
+        self.factory = RequestFactory()
         self.valid_data = {
-            'exp_date': '2025-12-31',
+            'exp_date': '2023-09-30',
             'holder': 'John Doe',
             'number': '4111111111111111',
             'cvv': '123',
+            'brand': 'Visa'
         }
 
     def test_valid_serializer(self):
-        serializer = CreditCardSerializer(data=self.valid_data)
+        request = self.factory.post('/your-api-endpoint/', data=self.valid_data)
+        serializer = CreditCardSerializer(data=self.valid_data, context={'request': request})
         self.assertTrue(serializer.is_valid())
 
     def test_invalid_exp_date(self):
         invalid_data = self.valid_data.copy()
-        invalid_data['exp_date'] = '2020-01-01'
-        serializer = CreditCardSerializer(data=invalid_data)
+        invalid_data['exp_date'] = '2021-01-01'  # Data de expiração no passado
+        request = self.factory.post('/your-api-endpoint/', data=invalid_data)
+        serializer = CreditCardSerializer(data=invalid_data, context={'request': request})
         self.assertFalse(serializer.is_valid())
-        self.assertIn('exp_date', serializer.errors)
-
-
-
