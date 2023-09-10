ed31f85f38605e318a80f6973394715ca875b95d
Sadinho <drdimsade@gmail.com>
2023-09-10 00:26:25 -0300

    validações inicio

--- api/core/serializers.py
+++ api/core/serializers.py
@@ -6,3 +6,30 @@ class CreditCardSerializer(serializers.ModelSerializer):
         model = CreditCard
         fields = ('id','exp_date', 'holder', 'number', 'cvv', 'brand')
 
+    def validate_exp_date(self, value):
+        # Validar a data de expiração
+        from datetime import date
+        current_date = date.today()
+        if value < current_date:
+            raise serializers.ValidationError("A data de expiração não pode ser anterior à data atual.")
+        return value
+
+    def validate_holder(self, value):
+        # Validar o titular
+        if len(value) < 3:
+            raise serializers.ValidationError("O titular deve ter mais de 2 caracteres.")
+        return value
+
+    def validate_number(self, value):
+        # Validar o número do cartão usando a biblioteca creditcard
+        card = CreditCard(card_number=value)
+        if not card.is_valid():
+            raise serializers.ValidationError("Número de cartão de crédito inválido.")
+        return value
+
+    def validate_cvv(self, value):
+        # Validar o CVV
+        if value and (len(value) < 3 or len(value) > 4 or not value.isdigit()):
+            raise serializers.ValidationError("O CVV deve ter entre 3 e 4 dígitos numéricos.")
+        return value
+
